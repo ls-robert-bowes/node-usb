d6d5e9921c98c5e44097583a2088d77cd7d3f6a3
tombertrand <tom.bertrand@lightspeedhq.com>
2021-06-10 17:14:19 -0400

    Remove HAVE_CLOCK_GETTIME definition to support El Capitan on MacOS

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -10,9 +10,17 @@ const moveDarwinConfig = async (folder, file) => {
       // Because node-usb is not properly getting the config.h on MacOS to set
       // PLATFORM_POSIX & other constants for building the bindings_usb.node file
       // @TODO fix this on https://github.com/lightspeedretail/node-usb
-      fs.createReadStream(`libusb/Xcode/config.h`).pipe(
-        fs.createWriteStream(`libusb_config/config.h`)
-      );
+      fs.readFile(`libusb/Xcode/config.h`, "utf8", (err, data) => {
+        if (err) {
+          return console.log("Error reading", err);
+        }
+        // Force HAVE_CLOCK_GETTIME to be undefined to support El Capitan
+        const text = data.replace("#define HAVE_CLOCK_GETTIME 1", "");
+
+        fs.writeFile(`libusb_config/config.h`, text, "utf8", (err) => {
+          if (err) return console.log("Error writing", err);
+        });
+      });
 
       console.log(chalk.blue("file moved"));
     } catch (err) {
